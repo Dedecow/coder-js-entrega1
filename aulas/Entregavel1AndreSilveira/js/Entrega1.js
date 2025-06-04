bb2a382cbd44f3dcd09e60f91a2852e40cdc9ecf
Dedecow <andrehbahia@gmail.com>
2025-06-04 07:47:54 -0300

    Salvando alterações locais antes do rebase

---
+++ aulas/Entregavel1AndreSilveira/js/Entrega1.js
@@ -0,0 +1,35 @@
+let nomeDigitado = '';
+let emailDigitado = '';
+let rendaDigitada = '';
+
+const tbName = document.getElementById('tb-name');
+
+function salvarNomeTabela() {
+    nomeDigitado = prompt('Digite seu nome.')||'Anônimo';
+    tbName.textContent = nomeDigitado;
+    console.log(
+        `Nome registrado para simulação de investimentos foi ${nomeDigitado}`
+    );
+}
+salvarNomeTabela();
+
+const tbEmail = document.getElementById('tb-email');
+
+function salvarEmailTabela() {
+    emailDigitado = prompt('Digite seu email.')||'email@email.com';
+    tbEmail.textContent = emailDigitado;
+    console.log(
+        `Email registrado para simulação de investimentos foi ${emailDigitado}`
+    );
+}
+salvarEmailTabela();
+
+const tbRenda = document.getElementById('tb-renda');
+
+function salvarRendaTabela(){
+    rendaDigitada=prompt('Digite sua renda.')||'';
+    tbRenda.textContent = rendaDigitada;
+    console.log(
+        `Renda registrada para simulação de investimentos foi ${rendaDigitada}`
+    )
+}
