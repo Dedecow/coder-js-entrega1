6180e539cd6de157868b88a785700764e36618f8
Dedecow <andrehbahia@gmail.com>
2025-06-20 08:32:10 -0300

    novo-titulo-innerText

---
+++ DOM/js/script.js
@@ -0,0 +1,9 @@
+let contenido = document.getElementById("contenido")
+contenido.innerHTML = `
+    <h1> Bem Vindo ao JavaScript </h1>
+    <p> Este é um parágrafo criado a partir do JavaScript </p>
+`
+let titulo = document.querySelector('h1')
+titulo.innerText = `
+    Este é o Novo Título e você ainda é Bem vindo!
+`
